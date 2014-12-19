3fb774222165982b5d88af5fcad4efa90c15d53f
themysteq <pawelsadlinki@gmail.com>
2014-12-20 00:50:17 +0100

    Dodanie wyjątków i wrzucania rekordów na stronę

--- sbd2_a/Page.cs
+++ sbd2_a/Page.cs
@@ -9,20 +9,42 @@ namespace sbd2_a
     class Page
     {
         static public uint records_per_page = 5;
+        public int counter = 0;
         private byte[] _buffer;
-        private Record[] _records = new Record[records_per_page];
+        private Record[] _records;
         public Page()
         {
-
+           _records = new Record[records_per_page];
+           for (int i = 0; i < records_per_page;i++ )
+           {
+               _records[i] = null;
+           }
         }
-        public void addRecordToPage()
+        public bool addRecordToPage(Record _record)
         {
+            if (counter < records_per_page)
+            {
+                _records[counter] = _record;
+                counter++;
+                return true;
+            }
+            else
+            {
+                throw new PageFullException("Page is full!");
 
+            }
         }
-        /*
+        Record[] getRecords()
+        {
+            return _records;
+        }
+        
+        
         public byte[] serializePageToBytes()
         {
-            byte[] serializingBuffer = null;
+            int to_allocate = 0;
+            to_allocate = sizeof()
+            byte[] serializingBuffer = new 
             foreach (Record record in this._records)
             {
                 
--- sbd2_a/Program.cs
+++ sbd2_a/Program.cs
@@ -20,6 +20,27 @@ namespace sbd2_a
             Console.WriteLine(test_result);
             Console.ReadLine();
              */
+            Page page = new Page();
+            for (int i = 0 ; i < 10 ; i++ )
+            {
+                byte[] valueToTest = Encoding.ASCII.GetBytes("LEL\0\0E");
+                Console.WriteLine(i);
+                valueToTest[0] = (byte)i;
+                Record record = new Record(valueToTest, 0);
+                bool ifInserted = false;
+                try
+                {
+                     ifInserted = page.addRecordToPage(record);
+                }
+                catch(PageFullException e)
+                {
+                    // podejmij próbe wrzucenia na inną stronę
+                    Console.WriteLine(e.Message);
+                }
+                
+                Console.WriteLine(ifInserted);
+            }
+            Console.ReadLine();
 
         }
     }
