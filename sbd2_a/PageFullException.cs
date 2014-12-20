adc7f4fa258c05ea393b0c29c43ebf6e1f6d594c
themysteq <pawelsadlinki@gmail.com>
2014-12-20 23:22:37 +0100

    Dodanie nietrakowanego pliku wyjątków

---
+++ sbd2_a/PageFullException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sbd2_a
+{
+    class PageFullException : Exception
+    {
+        public PageFullException() { }
+        public PageFullException(string message) : base(message) { }
+        public PageFullException(string message, Exception inner) : base(message, inner) { }
+    }
+}
